60fa6b2405522491cb5b231cf74f00c0487cf63c
[dyad] <red.octopus.624@gmail.com>
2025-07-30 13:03:03 +0800

    [dyad] Configuring base path for GitHub Pages deployment - wrote 2 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/gravity-chess"> {/* Add basename for GitHub Pages */}
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
